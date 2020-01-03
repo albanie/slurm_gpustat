58e3c5e422747dfaf318184d22142420814d0284
albanie <albanie@robots.ox.ac.uk>
2020-01-03 15:15:04 +0000

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="slurm_gpustat",
-    version="0.0.3.2",
+    version="0.0.3.3",
     entry_points={
         "console_scripts": [
             "slurm_gpustat=slurm_gpustat.slurm_gpustat:main",
