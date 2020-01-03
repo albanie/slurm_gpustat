7f1a815d7ec46d7e559bcfb465b2325848056fd1
Samuel Albanie <linc2488@arcus-htc-login03.arcus-htc.arc.local>
2020-01-03 09:33:14 +0000

    cleanup

--- slurm_gpustat/slurm_gpustat.py
+++ slurm_gpustat/slurm_gpustat.py
@@ -1,9 +1,13 @@
 """A simple tool for summarising GPU statistics on a slurm cluster
 """
-import os
+import random
 import argparse
 from collections import defaultdict
 import subprocess
+
+
+INACCESSIBLE = {"drain*", "down*", "drng", "drain", "down"}
+
 
 def split_node_str(node_str):
     node_str = node_str.strip()
@@ -33,7 +37,9 @@ def parse_node_names(node_str):
                     subnames = [f"{prefix}{subspec}"]
                 else:
                     start, end = subspec.split("-")
-                    subnames = [f"{prefix}{x}" for x in range(int(start), int(end) + 1)]
+                    num_digits = len(start)
+                    subnames = [f"{prefix}{str(x).zfill(num_digits)}"
+                                for x in range(int(start), int(end) + 1)]
                 names.extend(subnames)
     return names
 
@@ -57,28 +63,30 @@ def node_states():
 
 
 def parse_all_gpus(default_gpus=4):
-    cmd = "sinfo -o '%50N|%10G'"
+    cmd = "sinfo -o '%50N|%30G' --noheader"
     rows = parse_cmd(cmd)
-    resources = {}
+    resources = defaultdict(list)
     for row in rows:
-        node_str, resource_str = row.split("|")
-        if not resource_str.startswith("gpu"):
-            continue
-        tokens = resource_str.strip().split(":")
-        # if the number of GPUs is not specified, we assume it is `default_gpus`
-        if tokens[2] == "":
-            tokens[2] = default_gpus
-        gpu_type, gpu_count = tokens[1], int(tokens[2])
-        node_names = parse_node_names(node_str)
-        res = {name: {"type": gpu_type, "count": gpu_count} for name in node_names}
-        resources.update(res)
+        node_str, resource_strs = row.split("|")
+        for resource_str in resource_strs.split(","):
+            if not resource_str.startswith("gpu"):
+                continue
+            tokens = resource_str.strip().split(":")
+            # if the number of GPUs is not specified, we assume it is `default_gpus`
+            if tokens[2] == "":
+                tokens[2] = default_gpus
+            gpu_type, gpu_count = tokens[1], int(tokens[2])
+            node_names = parse_node_names(node_str)
+            for name in node_names:
+                resources[name].append({"type": gpu_type, "count": gpu_count})
     return resources
 
 
 def resource_by_type(resources):
     by_type = defaultdict(lambda: 0)
-    for spec in resources.values():
-        by_type[spec["type"]] += spec["count"]
+    for specs in resources.values():
+        for spec in specs:
+            by_type[spec["type"]] += spec["count"]
     return by_type
 
 
@@ -99,7 +107,7 @@ def summary(tag, resources=None, states=None):
         states = node_states()
     if tag == "accessible":
         res = {key: val for key, val in resources.items()
-               if states[key] not in {"drain*", "down*", "drng"}}
+               if states.get(key, "down") not in INACCESSIBLE}
     elif tag == "up":
         res = resources
     else:
@@ -117,14 +125,28 @@ def gpu_usage(resources):
         if len(tokens) < 3 or not tokens[0].startswith("gpu"):
             continue
         gpu_count_str, node_str, user = tokens
-        num_gpus = int(gpu_count_str.split(":")[1])
+        gpu_count_tokens = gpu_count_str.split(":")
+        num_gpus = int(gpu_count_tokens[-1])
+        if len(gpu_count_tokens) == 2:
+            gpu_type = None
+        elif len(gpu_count_tokens) == 3:
+            gpu_type = gpu_count_tokens[1]
         node_names = parse_node_names(node_str)
         for node_name in node_names:
-            gpu_type = resources[node_name]["type"]
+            node_gpu_types = [x["type"] for x in resources[node_name]]
+            if gpu_type is None:
+                if len(node_gpu_types) != 1:
+                    gpu_type = random.choice(node_gpu_types)
+                    msg = (f"cannot determine node gpu type for {user} on {node_name}"
+                           f" (guesssing {gpu_type})")
+                    print(f"WARNING >>> {msg}")
+                else:
+                    gpu_type = node_gpu_types[0]
             if gpu_type in usage[user]:
-                usage[user][gpu_type] += num_gpus
+                usage[user][gpu_type][node_name] += num_gpus
             else:
-                usage[user][gpu_type] = num_gpus
+                usage[user][gpu_type] = defaultdict(lambda: 0)
+                usage[user][gpu_type][node_name] += num_gpus
     return usage
 
 
@@ -132,25 +154,39 @@ def in_use(resources=None):
     if not resources:
         resources = parse_all_gpus()
     usage = gpu_usage(resources)
+    aggregates = {}
+    for user, subdict in usage.items():
+        aggregates[user] = {key: sum(val.values()) for key, val in subdict.items()}
     print("Usage by user:")
-    for user, subdict in sorted(usage.items(), key=lambda x: sum(x[1].values())):
+    for user, subdict in sorted(aggregates.items(), key=lambda x: sum(x[1].values())):
         total = f"total: {str(sum(subdict.values())):2s}"
         summary_str = ", ".join([f"{key}: {val}" for key, val in subdict.items()])
         print(f"{user:10s} [{total}] {summary_str}")
 
 
 def available(resources=None, states=None):
+    """Some systems allow users to share GPUs.  The logic below amounts to a conservative
+    estimate of how many GPUs are available.  The logic is essentially:
+
+      For each user that requests a GPU on a node, we assume that a new GPU is allocated
+      until all GPUs on the server are assigned.  If more GPUs than this are listed as
+      allocated by squeue, we assume any further GPU usage occurs by sharing GPUs.
+    """
     if not resources:
         resources = parse_all_gpus()
     if not states:
         states = node_states()
-    usage = gpu_usage(resources)
     res = {key: val for key, val in resources.items()
-           if states[key] not in {"drain*", "down*", "drng"}}
-    by_type = resource_by_type(res)
+           if states.get(key, "down") not in INACCESSIBLE}
+    usage = gpu_usage(resources=res)
     for subdict in usage.values():
-        for gpu_type, count in subdict.items():
-            by_type[gpu_type] -= count
+        for gpu_type, node_dicts in subdict.items():
+            for node_name, user_gpu_count in node_dicts.items():
+                resource_idx = [x["type"] for x in res[node_name]].index(gpu_type)
+                count = res[node_name][resource_idx]["count"]
+                count = max(count - user_gpu_count, 0)
+                res[node_name][resource_idx]["count"] = count
+    by_type = resource_by_type(res)
     total = sum(by_type.values())
     print(f"There are {total} gpus available:")
     for key, val in by_type.items():
@@ -174,7 +210,7 @@ def all_info():
 
 def main():
     parser = argparse.ArgumentParser(description="slurm_gpus tool")
-    parser.add_argument("--all", action="store_true")
+    parser.add_argument("--all", type=int, default=1)
     parser.add_argument("--in_use", action="store_true")
     parser.add_argument("--available", action="store_true")
     parser.add_argument("--verbose", action="store_true")
