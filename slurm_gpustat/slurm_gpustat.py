6ed712b5bc09c81a289a9df894b77713c020675e
Samuel Albanie <linc2488@arcus-htc-login02.arcus-htc.arc.local>
2020-01-04 14:44:01 +0000

    simplify file structure

--- slurm_gpustat/slurm_gpustat.py
+++ slurm_gpustat/slurm_gpustat.py
@@ -7,21 +7,147 @@ The tool can be used in two ways:
 """
 import argparse
 import ast
+import atexit
+import os
 import random
+import signal
 import subprocess
-import seaborn as sns
-import colored
+import sys
 import time
 from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
 
+import colored
 import numpy as np
-
-from slurm_gpustat.daemon import Daemon
+import seaborn as sns
 
 # SLURM states which indicate that the node is not available for submitting jobs
 INACCESSIBLE = {"drain*", "down*", "drng", "drain", "down"}
+
+
+class Daemon:
+    """A Generic linux daemon base class for python 3.x.
+
+    This code is a Python3 port of Sander Marechal's Daemon module:
+    http://web.archive.org/web/20131017130434/http://www.jejik.com/articles/
+    2007/02/a_simple_unix_linux_daemon_in_python/
+
+    It's a little difficult to credit the author of the Python3 port, since the code was
+    published anonymously. The original can be found here:
+    http://web.archive.org/web/20131101191715/http://www.jejik.com/files/examples/
+    daemon3x.py
+    """
+
+    def __init__(self, pidfile):
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        """Deamonize class. UNIX double fork mechanism."""
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write('fork #1 failed: {0}\n'.format(err))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.chdir('/')
+        os.setsid()
+        os.umask(0)
+
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write('fork #2 failed: {0}\n'.format(err))
+            sys.exit(1)
+
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = open(os.devnull, 'r')
+        so = open(os.devnull, 'a+')
+        se = open(os.devnull, 'a+')
+
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        # write pidfile
+        atexit.register(self.delpid)
+
+        pid = str(os.getpid())
+        with open(self.pidfile, 'w+') as f:
+            f.write(pid + '\n')
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        """Start the daemon."""
+
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            with open(self.pidfile, 'r') as pf:
+
+                pid = int(pf.read().strip())
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile {0} already exists. Is the daemon already running?\n"
+            sys.stderr.write(message.format(self.pidfile))
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """Stop the daemon."""
+
+        # Get the pid from the pidfile
+        try:
+            with open(self.pidfile, 'r') as pf:
+                pid = int(pf.read().strip())
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile {0} does not exist. Daemon not running?\n"
+            sys.stderr.write(message.format(self.pidfile))
+            return  # not an error in a restart
+
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError as err:
+            e = str(err.args)
+            if e.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print(str(err.args))
+                sys.exit(1)
+
+    def restart(self):
+        """Restart the daemon."""
+        self.stop()
+        self.start()
+
+    def run(self):
+        """You should override this method when you subclass Daemon.
+        It will be called after the process has been daemonized by 
+        start() or restart()."""
+        raise NotImplementedError("Must override this class")
 
 
 class GPUStatDaemon(Daemon):
