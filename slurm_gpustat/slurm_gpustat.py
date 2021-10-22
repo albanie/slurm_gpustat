c45a5b319cd9277e0c6b4e9df189ada3ce7ad541
Relento <relent0@outlook.com>
2021-10-21 17:03:09 -0700

    Change to double quotes.

--- slurm_gpustat/slurm_gpustat.py
+++ slurm_gpustat/slurm_gpustat.py
@@ -523,12 +523,12 @@ def gpu_usage(resources: dict, partition: Optional[str] = None) -> dict:
     Returns:
         (dict): a summary of resources organised by user (and also by node name).
     """
-    version_cmd = 'sinfo -V'
-    slurm_version = parse_cmd(version_cmd, split=False).split(' ')[1]
-    if slurm_version.startswith('17'):
-       resource_flag = 'gres'
+    version_cmd = "sinfo -V"
+    slurm_version = parse_cmd(version_cmd, split=False).split(" ")[1]
+    if slurm_version.startswith("17"):
+       resource_flag = "gres"
     else:
-       resource_flag = 'tres-per-node'
+       resource_flag = "tres-per-node"
     cmd = f"squeue -O {resource_flag}:100,nodelist:100,username:100,jobid:100 --noheader"
     if partition:
         cmd += f" --partition={partition}"
@@ -543,7 +543,7 @@ def gpu_usage(resources: dict, partition: Optional[str] = None) -> dict:
         gpu_count_str, node_str, user, jobid = tokens
         gpu_count_tokens = gpu_count_str.split(":")
         if not gpu_count_tokens[-1].isdigit():
-            gpu_count_tokens.append('1')
+            gpu_count_tokens.append("1")
         num_gpus = int(gpu_count_tokens[-1])
         # get detailed job information, to check if using bash
         detailed_output = parse_cmd(detailed_job_cmd % jobid, split=False)
